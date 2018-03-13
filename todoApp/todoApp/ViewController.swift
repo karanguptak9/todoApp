f830cce9ea36fa664f2d1cda37f6c340bd2f827a
Karan Gupta <kgupta@mail.sfsu.edu>
2018-03-12 23:53:18 -0700

    Adding check marks

--- todoApp/todoApp/ViewController.swift
+++ todoApp/todoApp/ViewController.swift
@@ -36,7 +36,16 @@ class ViewController: UITableViewController {
         
         return cell
     }
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row < todoVars.count {
+            let item = todoVars[indexPath.row]
+            item.done = !item.done
+            
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
